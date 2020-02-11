28a8e6188bc8c61fb1e274953175cff6c2ab396a
martamatos <marta.ra.matos@gmail.com>
2020-02-11 12:24:36 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="yaml2sbml",
+    version="0.1.0",
+    author="Jakob Vanhoefer, Marta R. A. Matos",
+    author_email="marta.ra.matos@gmail.com",
+    description="A small package to convert ODEs specified in a yaml file to SBML.",
+    url="https://github.com/martamatos/yaml2sbml",
+    packages=setuptools.find_packages(),
+    install_requires=["python-libsbml>=5.18.0",
+                      "PyYAML>=5.3"],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3.6+",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+)
