99cc55d2811a7be441283150e140bb5baae0a637
martamatos <marta.ra.matos@gmail.com>
2020-02-11 12:48:53 +0100

    Fix: fixed path to test files so it works with travisCI

--- yaml2sbml/tests/testYaml2SBML.py
+++ yaml2sbml/tests/testYaml2SBML.py
@@ -9,37 +9,41 @@ class TestYamlImport(unittest.TestCase):
     TestCase class for testing ODE import from a generic yaml file and convertion to SBML.
     """
 
+    def setUp(self):
+        this_dir, this_filename = os.path.split(__file__)
+        self.test_folder = os.path.join(this_dir, 'test_yaml2sbml')
+
     def test_yaml_import(self):
-        ode_file = os.path.join('test_yaml2sbml', 'ode_input1.yaml')
-        expected_result_file = os.path.join('test_yaml2sbml', 'true_sbml_output1.xml')
+        ode_file = os.path.join(self.test_folder, 'ode_input1.yaml')
+        expected_result_file = os.path.join(self.test_folder, 'true_sbml_output1.xml')
 
         sbml_contents = parse_yaml(ode_file)
 
         with open(expected_result_file, 'r') as f_in:
             expected_sbml_contents = f_in.read()
 
-        with open(os.path.join('test_yaml2sbml', 'sbml_test.xml'), 'w') as f_out:
+        with open(os.path.join(self.test_folder, 'sbml_test.xml'), 'w') as f_out:
             f_out.write(sbml_contents)
 
         self.assertEqual(expected_sbml_contents, sbml_contents)
 
-        os.remove(os.path.join('test_yaml2sbml', 'sbml_test.xml'))
+        os.remove(os.path.join(self.test_folder, 'sbml_test.xml'))
 
     def test_yaml_import_observables(self):
-        ode_file = os.path.join('test_yaml2sbml', 'ode_input2.yaml')
-        expected_result_file = os.path.join('test_yaml2sbml', 'true_sbml_output2.xml')
+        ode_file = os.path.join(self.test_folder, 'ode_input2.yaml')
+        expected_result_file = os.path.join(self.test_folder, 'true_sbml_output2.xml')
 
         sbml_contents = parse_yaml(ode_file)
 
         with open(expected_result_file, 'r') as f_in:
             expected_sbml_contents = f_in.read()
 
-        with open(os.path.join('test_yaml2sbml', 'sbml_test.xml'), 'w') as f_out:
+        with open(os.path.join(self.test_folder, 'sbml_test.xml'), 'w') as f_out:
             f_out.write(sbml_contents)
 
         self.assertEqual(expected_sbml_contents, sbml_contents)
 
-        os.remove(os.path.join('test_yaml2sbml', 'sbml_test.xml'))
+        os.remove(os.path.join(self.test_folder, 'sbml_test.xml'))
 
 
 if __name__ == '__main__':
