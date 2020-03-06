b514d03ea82493c016217bf0d5606683acb898b7
Jakob Vanhoefer <ja131va@yahoo.de>
2020-03-06 11:15:39 +0100

    version +=1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
     name="yaml2sbml",
-    version="0.1.0",
+    version="0.1.1",
     author="Jakob Vanhoefer, Marta R. A. Matos",
     author_email="marta.ra.matos@gmail.com",
-    description="A small package to convert ODEs specified in a yaml file to SBML.",
+    description="A small package to convert ODEs specified in a yaml file to SBML/PEtab.",
     url="https://github.com/martamatos/yaml2sbml",
     packages=setuptools.find_packages(),
     install_requires=["python-libsbml>=5.18.0",
