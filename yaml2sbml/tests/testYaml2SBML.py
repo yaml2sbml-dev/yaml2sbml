80d30469630506eb1aec6ddca3fa60692d1fb846
Jakob Vanhoefer <ja131va@yahoo.de>
2020-04-09 12:35:36 +0200

    split up tests partially adresses #18

--- yaml2sbml/tests/testYaml2SBML.py
+++ yaml2sbml/tests/testYaml2SBML.py
@@ -2,10 +2,9 @@ import os
 import unittest
 
 from yaml2sbml.yaml2sbml import parse_yaml
-import yaml2sbml.yaml2PEtab as yaml2PEtab
 
 
-class TestYamlImport(unittest.TestCase):
+class TestYaml2SBML(unittest.TestCase):
     """
     TestCase class for testing ODE import from a generic yaml file and conversion to SBML.
     """
@@ -49,24 +48,8 @@ class TestYamlImport(unittest.TestCase):
 
         os.remove(os.path.join(self.test_folder, 'sbml_test.xml'))
 
-    def test_petab_export(self):
-        """
-        Test PEtab export
-        """
-        ode_file = os.path.join(self.test_folder, 'ode_input2.yaml')
-
-        yaml2PEtab.yaml2petab(ode_file,
-                              self.test_folder,
-                              'sbml_test.xml')
-
-        yaml2PEtab.validate_petab_tables(os.path.join(self.test_folder, 'sbml_test.xml'),
-                                         self.test_folder)
-
-        for file in ['observable_table.tsv', 'parameter_table.tsv', 'condition_table.tsv', 'sbml_test.xml']:
-            os.remove(os.path.join(self.test_folder, file))
-
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTest(TestYamlImport())
+    suite.addTest(TestYaml2SBML())
     unittest.main()
