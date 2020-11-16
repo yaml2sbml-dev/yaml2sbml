4fca20ac1bf9784da964111e25e007625405e7cf
Jakob Vanhoefer <jakob.vanhoefer@uni-bonn.de>
2020-11-16 17:55:49 +0100

    fix observables

--- yaml2sbml/yaml_validation.py
+++ yaml2sbml/yaml_validation.py
@@ -19,14 +19,30 @@ def validate_yaml(yaml_file: str):
     Raises:
 
     """
-    # read in SCHEMA
-    with open(SCHEMA, 'r') as f_in:
-        yaml_contents = f_in.read()
-        schema = yaml.full_load(yaml_contents)
-
     # read in yaml_file
     with open(yaml_file, 'r') as f_in:
         yaml_contents = f_in.read()
         yaml_in = yaml.full_load(yaml_contents)
 
-    jsonschema.validate(instance=yaml_in, schema=schema)
+    _validate_yaml_from_dict(yaml_in)
+
+
+def _validate_yaml_from_dict(yaml_dict: dict):
+    """
+    Validates the syntax of the yaml file, using a dict as input.
+
+    Arguments:
+        yaml_dict: yaml model as dict.
+
+    Returns:
+        jsonschema.validate
+
+    Raises:
+
+    """
+    # read in SCHEMA
+    with open(SCHEMA, 'r') as f_in:
+        yaml_contents = f_in.read()
+        schema = yaml.full_load(yaml_contents)
+
+    jsonschema.validate(instance=yaml_dict, schema=schema)
