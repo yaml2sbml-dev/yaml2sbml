56ede887a6892f7bf4b98b6bcf7361d92e1cff37
Jakob Vanhoefer <jakob.vanhoefer@uni-bonn.de>
2021-01-28 17:43:10 +0100

    refactor Examples, add FSP example

---
+++ doc/examples/Finite_State_Projection/utils.py
@@ -0,0 +1,123 @@
+import amici
+import amici.plotting
+
+import importlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+
+
+def plot_AMICI(sbml_dir: str,
+               t: np.ndarray,
+               r_max: int,
+               p_max: int):
+
+    n_t = len(t)
+    # compile and Simulate model
+    simulation, model = compile_and_simulate(sbml_dir,
+                                             t,
+                                             r_max,
+                                             p_max)
+
+    def get_obs_idx_by_id(obs_id: str):
+        for idx, obs in enumerate(model.getObservableIds()):
+            if obs == obs_id:
+                return idx
+        raise IndexError(f'No observable with id {obs_id}')
+
+    for i in range(n_t):
+
+        r_marginal = [simulation.y[i, get_obs_idx_by_id(f'x_r{r}')] for r in range(r_max)]
+        p_marginal = [simulation.y[i, get_obs_idx_by_id(f'x_p{p}')] for p in range(p_max)]
+        # rna
+        plt.subplot(n_t, 2, 2*i+1)
+        plt.fill_between(np.arange(r_max), 0, r_marginal, facecolor='blue', alpha=0.5)
+        plt.plot(r_marginal)
+        plt.ylabel(f't={int(t[i])}')
+        plt.yticks([])
+        plt.xlim(0, r_max-1)
+        plt.ylim(0, 0.3)
+
+        if i < n_t-1:
+            plt.tick_params(labelbottom=False)
+        else:
+            plt.xlabel('mRNA abundance')
+
+        # protein
+        plt.subplot(n_t, 2, 2*(i+1))
+        plt.fill_between(np.arange(p_max), 0, p_marginal, facecolor='blue', alpha=0.5)
+        plt.plot(p_marginal)
+        plt.yticks([ ])
+        plt.xlim(0, p_max-1)
+        plt.ylim(0, 0.2)
+        if i < n_t-1:
+            plt.tick_params(labelbottom=False)
+        else:
+            plt.xlabel('protein abundance')
+
+    plt.subplots_adjust(wspace=0.07, hspace=0.1)
+    plt.show()
+
+
+def compile_and_simulate(sbml_dir: str,
+                         t: np.ndarray,
+                         r_max: int,
+                         p_max: int):
+
+    model_name = 'Gene_regulation_FSP'
+
+    # define marginal rna concentrations
+    observables_rna = {f'x_r{r}': get_marginal_rna_obs(r, p_max)
+                       for r in range(r_max)}
+
+    # define marginal protein concentrations
+    observables_protein = {f'x_p{p}': get_marginal_protein_obs(p, r_max)
+                           for p in range(p_max)}
+
+    observables = {**observables_rna, **observables_protein}
+
+    sbml_importer = amici.sbml_import.SbmlImporter(sbml_dir)
+    sbml_importer.sbml2amici(
+        model_name=model_name,
+        output_dir=os.path.join('amici_models', model_name),
+        observables=observables)
+
+    # Import the compiled model.
+    sys.path.insert(0,
+                    os.path.abspath(os.path.join('amici_models', model_name)))
+    model_module = importlib.import_module(model_name)
+    model = model_module.getModel()
+
+    # Simulate.
+    model.setTimepoints(t)
+    solver = model.getSolver()
+    simulation = amici.runAmiciSimulation(model, solver)
+
+    return simulation, model
+
+
+def get_marginal_rna_obs(r: int, p_max: int):
+    """
+    Returns a dict of containing the observable for the given marginalized
+    rna abundance of r in AMICI.
+    """
+    marginal = ''
+    for p in range(p_max-1):
+        marginal += f'x_{r}_{p} + '
+    marginal += f'x_{r}_{p_max-1}'
+
+    return {'name': f'x_r{r}', 'formula': marginal}
+
+
+def get_marginal_protein_obs(p: int, r_max: int):
+    """
+    Returns a dict of containing the observable for the given marginalized
+    protein abundance of p in AMICI.
+    """
+    marginal = ''
+    for r in range(r_max-1):
+        marginal += f'x_{r}_{p} + '
+    marginal += f'x_{r_max-1}_{p}'
+
+    return {'name': f'x_p{p}', 'formula': marginal}
--- doc/examples/README.md
+++ doc/examples/README.md
@@ -4,18 +4,20 @@ The [`yaml2sbml`](https://github.com/yaml2sbml-dev/yaml2sbml) package translates
 
 # Scope of the Notebooks
 
-* [Lotka Volterra (Python) ](./Lotka_Volterra_python/Lotka_Volterra.ipynb)
-    * Introduces the input format, syntax & capabilities of `yaml2sbml`
-    * Showcases ODE simulation & parameter fitting using [AMICI](https://github.com/AMICI-dev/AMICI) & [pyPESTO](https://github.com/ICB-DCM/pyPESTO)
-* [Lotka Volterra CLI](./Lotka_Volterra_CLI/Lotka_Volterra_CLI.ipynb)
-    * Shows the command line interface of `yaml2sbml`
-* [Lotka Volterra Model Editor](.Lotka_Volterra_Model_Editor/Lotka_Volterra_Model_Editor.ipynb)
-    * Shows model construction using the model editor of `yaml2sbml`
-* [Sorensen](./Sorensen.yaml2sbml_Sorensen.ipynb)
+* [Lotka Volterra Notebooks]
+    * [Lotka Volterra (Python) ](./Lotka_Volterra/Lotka_Volterra_python/Lotka_Volterra.ipynb)
+        * Introduces the input format, syntax & capabilities of `yaml2sbml`
+        * Showcases ODE simulation & parameter fitting using [AMICI](https://github.com/AMICI-dev/AMICI) & [pyPESTO](https://github.com/ICB-DCM/pyPESTO)
+    * [Lotka Volterra CLI](./Lotka_Volterra/Lotka_Volterra_CLI/Lotka_Volterra_CLI.ipynb)
+        * Shows the command line interface of `yaml2sbml`
+    * [Lotka Volterra Model Editor](./Lotka_Volterra/Lotka_Volterra_Model_Editor/Lotka_Volterra_Model_Editor.ipynb)
+        * Shows model construction using the model editor of `yaml2sbml`
+* [Sorensen](./Sorensen/yaml2sbml_Sorensen.ipynb)
     * Application example, model of glucose and insulin metabolism
     * Shows the extension of an existing YAML model by the model editor
-* Pom1p (planned, work in progress)
-    * Application example, discretization of a PDE model of Pom1 gradient formation
+* [Finite State Projection](./Finite_State_Projection/Finite_State_Projection.ipynb)
+    * Application example of a stochastic gene transcription model having hundreds of states.
+    * Generates a complex and realistic model within a few lines of python.
 * [Format Features](./Format_Features/Format_Features.ipynb)
     * several didactic examples, that show individual features of `yaml2sbml`:
         * Time-dependent right hand sides
