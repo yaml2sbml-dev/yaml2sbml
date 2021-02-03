cd5324d4ef1934e90090242410af4a7fed030cc6
Jakob Vanhoefer <jakob.vanhoefer@uni-bonn.de>
2021-02-03 18:22:51 +0100

    add long descrition

--- setup.py
+++ setup.py
@@ -9,9 +9,14 @@ ENTRY_POINTS = {
     ]
 }
 
+# read version
 with open(os.path.join(os.path.dirname(__file__),
           "yaml2sbml", "version.py")) as f:
     version = f.read().split('\n')[0].split("'")[-2]
+
+# read README as long_description
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="yaml2sbml",
@@ -20,6 +25,8 @@ setuptools.setup(
     author_email="marta.ra.matos@gmail.com",
     description="A small package to convert ODEs specified in "
                 "a yaml file to SBML/PEtab.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/martamatos/yaml2sbml",
     packages=setuptools.find_packages(),
     install_requires=["python-libsbml>=5.18.0",
