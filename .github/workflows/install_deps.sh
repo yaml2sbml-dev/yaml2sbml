f779df1044358e4dd91a64ca9b193deed539b310
yannikschaelte <yannik.schaelte@gmail.com>
2021-02-04 21:58:00 +0100

    add tox

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -18,9 +18,6 @@ for par in "$@"; do
       # for amici
       sudo apt-get install \
         swig3.0 libatlas-base-dev libhdf5-serial-dev
-
-      # install latest amici release
-      pip install amici
     ;;
 
     *)
