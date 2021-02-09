fe41fc69ad83908698c20d399a32cd4a8aaeb409
Jakob Vanhoefer <jakob.vanhoefer@uni-bonn.de>
2021-02-09 23:54:43 +0100

    test CLI within pytest

---
+++ tests/test_CLI.py
@@ -0,0 +1,40 @@
+import os
+import pytest
+from pytest_console_scripts import script_runner
+
+
+def test_yaml2sbml_cli(script_runner):
+    """Test the command line command `yaml2sbml`."""
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    yaml_dir = os.path.join(path, 'test_yaml2sbml/ode_input1.yaml')
+    sbml_dir = os.path.join(path, 'test_sbml.xml')
+
+    script_runner.run('yaml2sbml', yaml_dir, sbml_dir)
+
+    os.remove(sbml_dir)
+
+
+def test_yaml2petab_cli(script_runner):
+    """Test the command line command yaml2petab."""
+    path = os.path.dirname(os.path.abspath(__file__))
+    yaml_dir = os.path.join(path, 'test_yaml2sbml/ode_input2.yaml')
+    output_dir = os.path.join(path, 'test_output')
+    model_name = 'petab_test_sbml.xml'
+
+    # run with no optional Arguments
+    script_runner.run('yaml2petab', yaml_dir, output_dir, model_name)
+
+    # run with optional Arguments
+    script_runner.run('yaml2petab', yaml_dir, output_dir, model_name, '-y test_yaml.yml')
+
+    # TODO delete the generated files
+
+
+def test_yaml2sbml_validate_cli(script_runner):
+    """Test the command line command yaml2sbml_validate"""
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    yaml_dir = os.path.join(path, 'test_yaml2sbml/ode_input1.yaml')
+
+    script_runner.run('yaml2sbml_validate', yaml_dir)
